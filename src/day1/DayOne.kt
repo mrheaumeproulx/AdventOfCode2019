2d5c86d768a553e34ab06bb9d16993609c76faaf
Maxime Rheaume-Proulx <maxime.r.proulx@gmail.com>
2019-12-25 15:14:46 -0500

    First commit

--- src/day1/DayOne.kt
+++ src/day1/DayOne.kt
@@ -1,4 +1,21 @@
-package Day1
+package day1
 
-class DayOne {
+import java.io.File
+
+fun main(args: Array<String>) {
+    var modulesFuel: ArrayList<Int> = ArrayList()
+    var modules = File("src/day1/inputs.txt").readLines()
+
+    modules.forEach{ modulesFuel.add(CalculateFuel(it.toInt())) }
+
+    var totalFuel = modulesFuel.sum()
+    print(totalFuel)
+}
+
+fun CalculateFuel(mass: Int): Int {
+    var fuel = (mass / 3) - 2
+    if(fuel < 1)
+        return 0
+    fuel += CalculateFuel(fuel)
+    return fuel
 }
